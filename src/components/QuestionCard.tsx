4c4d8524b8ce49c78b0d1bc5d3b29c401c568017
Eduardo Hidalgo <polilla2802@gmail.com>
2022-03-17 14:58:49 -0500

    QuestionCard component to render questions/answers

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+// Types
+import { UserAnswer } from "../App";
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: UserAnswer | undefined;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="QuestionNumber">
+      Question {questionNumber} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div key={answer}>
+          <button
+            disabled={userAnswer ? true : false}
+            value={answer}
+            onClick={callback}
+          >
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default QuestionCard;
